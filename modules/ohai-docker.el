59d329e7d15608d778d61e39c18de8c1b1131f90
Oles Savluk <oles@qwil.io>
2018-12-31 18:47:47 +0200

    and finally I can use my fork of docker-tramp

--- modules/ohai-docker.el
+++ modules/ohai-docker.el
@@ -3,10 +3,15 @@
 
 (require 'ohai-package)
 
+(use-package docker-tramp
+  :straight (docker-tramp
+             :type git
+             :host github
+             :repo "olessavluk/docker-tramp.el"
+             :branch "multi-hops"))
+
 (use-package docker
   :bind ("C-c d" . docker))
-
-(use-package docker-tramp)
 
 (use-package dockerfile-mode
   :commands dockerfile-mode
