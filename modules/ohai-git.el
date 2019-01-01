0b1ecdec9072cfce7e626493a2a8542382fba6eb
Oles Savluk <oles@qwil.io>
2019-01-01 20:32:21 +0200

    fullscreen magit please

--- modules/ohai-git.el
+++ modules/ohai-git.el
@@ -26,6 +26,9 @@
 ;; See http://magit.github.io/ for instructions.
 (use-package magit
   :commands magit-status
+  :init
+  ;; https://github.com/magit/magit/issues/1953#issuecomment-221134023
+  (setq magit-display-buffer-function #'magit-display-buffer-fullframe-status-v1)
   :bind ("C-x g" . magit-status))
 
 ;; Use M-x gist-buffer or M-x gist-region to create a gist
